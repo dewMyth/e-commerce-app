cdcbabfad78baaaaa52b9b8debf7cc61662c57e9
dewMyth <63700919+dewMyth@users.noreply.github.com>
2022-01-12 12:29:30 +0530

    added password encryption using AES from Crypto JS

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 
+const CryptoJS = require("crypto-js");
+
 //Register User
 router.post("/register", async (req, res) => {
   console.log(req.body);
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET_KEY
+    ).toString(),
   });
 
   try {
