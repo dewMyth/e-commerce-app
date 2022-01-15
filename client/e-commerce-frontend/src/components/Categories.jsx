8c0e936ee263637baadfcaf629b60c14c003cf98
dewMyth <63700919+dewMyth@users.noreply.github.com>
2022-01-15 16:09:11 +0530

    added category

---
+++ client/e-commerce-frontend/src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+import { categories } from "../data";
+import CategoryItem from "./CategoryItem";
+
+const Conatiner = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+const Categories = () => {
+  return (
+    <Conatiner>
+      {categories.map((item) => (
+        <CategoryItem item={item} />
+      ))}
+    </Conatiner>
+  );
+};
+
+export default Categories;
