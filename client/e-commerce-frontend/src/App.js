77a9cc44e8c3b959226570b7337a63fe9f17d28a
dewmith <dewmith@hsenidoutsourcing.com>
2022-01-24 12:09:45 +0530

    added login and register ui + backend modifications ???

--- client/e-commerce-frontend/src/App.js
+++ client/e-commerce-frontend/src/App.js
@@ -1,11 +1,15 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
   // return <Home />;
   // return <ProductList />;
-  return <Product />;
+  // return <Product />;
+  // return <Register />;
+  return <Login />;
 };
 
 export default App;
